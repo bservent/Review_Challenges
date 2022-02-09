0992660cfc18b9fbb05964bfe7904af6c2ec94bf
Brittany Servent <bservent@gmail.com>
2022-02-09 15:40:25 -0800

    warm up probs

---
+++ warmup.js
@@ -0,0 +1,30 @@
+/* print all even numbers 0-10*/
+/* function evenNumbers(nums) {
+  for(let i = 1; i < nums.length; i++) {
+    if(nums[i] % 2 === 0) {
+      console.log(nums[i])
+    }
+  }
+}
+
+evenNumbers([1,2,3,4,5,6,7,8,9,10]); */
+
+/* print a table containing multiplication tables */
+/* function multiplicationTable(num) {
+  for(let i = 1; i <= 10; i++) {
+    const result = i * num;
+    console.log(`${num} * ${i} = ${result}`)
+  }
+}
+
+multiplicationTable(2); */
+
+/* kilometers to mile */
+
+/* function kilometersToMile(kil) {
+  let result = kil / 1.6;
+  console.log(result)
+  return result;
+}
+
+kilometersToMile(2); */
