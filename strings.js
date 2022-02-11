cbe812da86657f122a8cf8dbf5e849bffaa16035
Brittany Servent <bservent@gmail.com>
2022-02-10 21:04:17 -0800

    double string prob

---
+++ strings.js
@@ -0,0 +1,17 @@
+/* Repeating Letters
+Create a function that takes a string and returns a string in which each character is repeated once.
+
+Examples
+doubleChar("String") ➞ "SSttrriinngg"
+
+doubleChar("Hello World!") ➞ "HHeelllloo  WWoorrlldd!!"
+
+doubleChar("1234!_ ") ➞ "11223344!!__  " */
+
+/* function doubleChar(string) {
+  var result = string.split('').map(char => char + char).join('')
+  console.log(result);
+}
+
+doubleChar('yes i will do great!'); */
+
