447f215ec4ecedee43f56867543119d9f28b8513
bservent <bservent@gmail.com>
2022-02-24 12:31:42 -0800

    match with concat

--- strings.js
+++ strings.js
@@ -148,7 +148,7 @@ removeABC("hello world!") ➞ null
 
 removeABC("") ➞ null */
 
-function removeABC(string) {
+/* function removeABC(string) {
   if(string === '') {
     return null;
   }else{
@@ -158,4 +158,29 @@ function removeABC(string) {
 }
 
 console.log(removeABC(''));
-console.log(removeABC('I love this alot bc!'));
+console.log(removeABC('I love this alot bc!')); */
+
+/* Match the Last Item
+Create a function that takes an array of items and checks if the last item matches the rest of the array concatenated together.
+
+Examples
+matchLastItem(["rsq", "6hi", "g", "rsq6hig"]) ➞ true
+// The last item is the rest joined.
+
+matchLastItem([1, 1, 1, "11"]) ➞ false
+// The last item should be "111".
+
+matchLastItem([8, "thunder", true, "8thundertrue"]) ➞ true */
+
+function matchLastItem(array) {
+  let concatStr = array[0] + array[1] + array[2];
+  if(concatStr === array[3]) {
+    return true
+  }else{
+    return false
+  }
+}
+
+console.log(matchLastItem([8, "thunder", true, "8thundertrue"]))
+console.log(matchLastItem([1, 1, 1, "11"]))
+console.log(matchLastItem(["rsq", "6hi", "g", "rsq6hig"]))
